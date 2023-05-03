617bbbe27256d6f0eb708ec240c53665ec63fe66
devscopee <scopedeveloper31@gmail.com>
2023-05-04 00:09:26 +0400

    kurd gorkem

--- add-balance.php
+++ add-balance.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='css/add-balance.css'>
     <link rel='stylesheet' href='css/global.css'>
     <link rel='stylesheet' href='css/media.css'>
-    <link rel='stylesheet' href='css/add-balance.css'>
-    <title>Maraz Game</title>
 </head>
 <body>
+    <div id="preloader">
+        <img src="img/logo.png" alt="">
+    </div>
+
     <?php
-        include('inc/header.php')
+    include('inc/header.php')
     ?>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/main.js"></script>
 </body>
+
 </html>
